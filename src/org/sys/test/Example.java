641351b6e24664e7e5cdc2de3ac90931b0fb72be
Anitha1591 <ranithacse@gmail.com>
2023-02-06 20:11:16 +0530

    test code

---
+++ src/org/sys/test/Example.java
@@ -0,0 +1,28 @@
+package org.sys.test;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		double i=5.1;
+		
+		if(i==10){
+			
+			System.out.println("i is even number");
+		}
+		else if (i==5){
+			
+			System.out.println("i is odd number");
+			
+		}
+		else {
+			
+			System.out.println("i is neither even nor odd");
+	}
+		
+		for(int j=0;j<=6;j++)
+		{
+			System.out.println("The value of j is" +j);
+		}
+}
+}
